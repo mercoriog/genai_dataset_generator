cf884070120bba87e30fef5a381a4e107aaf9363
mercoriog <98778953+mercoriog@users.noreply.github.com>
2024-04-17 11:43:11 +0200

    insert script in gui

--- src/gui/script/generator.py
+++ src/gui/script/generator.py
@@ -1,22 +1,18 @@
 from PIL import Image as image
 import os
-from datasets import Dataset, Image
 
 TRAINING_RESOLUTION = (512, 512)
-DATASET_FOLDER = os.path.abspath(".\\Desktop\\Dataset\\train")
 
 def resizeImages(dataset):
     dataset["pixel_values"] = [image.convert("RGB").resize(TRAINING_RESOLUTION) for image in dataset["image"]]
     return dataset
 
-def getFilesPath():
-    files = os.listdir(DATASET_FOLDER)
+def getFilesPath(folder):
+    files = os.listdir(folder)
     # filtro le immagini 
     image_files = [f for f in files if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
-    image_files = [os.path.join(DATASET_FOLDER, im) for im in image_files]
+    image_files = [os.path.join(folder, im) for im in image_files]
     return image_files
 
 def generateDatset():
     pass
-    
-image_files = getFilesPath()
