204aefa880bbeec171bb258f46c98d3a44b981b0
mercoriog <98778953+mercoriog@users.noreply.github.com>
2024-05-08 12:48:34 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,13 +17,25 @@ fi
 python_version=$(python3 --version 2>&1)
 echo "Current python3 version: $python_version"
 
+# Check if pip3 is installed.
+if ! command -v pip3 &> /dev/null
+then
+	echo "pip3 not installed. Start installation..."
+
+	# Install pip3.
+	sudo apt install python3-pip
+
+	echo "pip3 installation done."
+else
+	echo "pip3 found."
+fi
+
 # Check venv installed
 if dpkg -s python3-venv &> /dev/null; then
     echo "venv found."
 else
     echo "venv non installed. Start installation..."
-    sudo apt update
-    sudo apt install python3-venv -y
+    spip install --user virtualenv
     echo "venv installation done."
 fi
 
@@ -37,25 +49,18 @@ desired_version="3.9.18"
 # Check if python version is 3.9.18
 if [[ "$python_version" != *"$desired_version"* ]]; then
 	# Upgrade python to 3.9.18
-	sudo add-apt-repository -y ppa:deadsnakes/ppa
-    sudo apt-get update
-    sudo apt-get install -y python3.9
+	# Installa una specifica versione di Python con pyenv
+    curl https://pyenv.run | bash
+    export PATH="$HOME/.pyenv/bin:$PATH"
+    eval "$(pyenv init --path)"
+
+    pyenv install $desired_version
+    pyenv global $desired_version
+
 else
 	echo "Correct python3 version installed. [Python3 $desired_version]"
 fi
 
-# Check if pip3 is installed.
-if ! command -v pip3 &> /dev/null
-then
-	echo "pip3 not installed. Start installation..."
-
-	# Install pip3.
-	sudo apt install python3-pip
-
-	echo "pip3 installation done."
-else
-	echo "pip3 found."
-fi
 
 pip install -r requirements.txt
 
