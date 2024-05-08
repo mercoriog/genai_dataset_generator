8da420958a96955b069b6a04b2543fba2204cfea
mercoriog <98778953+mercoriog@users.noreply.github.com>
2024-05-08 12:44:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,16 @@ fi
 python_version=$(python3 --version 2>&1)
 echo "Current python3 version: $python_version"
 
+# Check venv installed
+if dpkg -s python3-venv &> /dev/null; then
+    echo "venv found."
+else
+    echo "venv non installed. Start installation..."
+    sudo apt update
+    sudo apt install python3-venv -y
+    echo "venv installation done."
+fi
+
 python3 -m venv genaienv
 
 source genaienv/Scripts/activate
@@ -27,21 +37,9 @@ desired_version="3.9.18"
 # Check if python version is 3.9.18
 if [[ "$python_version" != *"$desired_version"* ]]; then
 	# Upgrade python to 3.9.18
-	sudo apt update
-	sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
-	
-	# Download and compile
-	cd /tmp
-	wget https://www.python.org/ftp/python/$desired_version/Python-$desired_version.tgz
-	tar -xf Python-$desired_version.tgz
-	cd Python-$desired_version
-	./configure --enable-optimizations
-	make -j$(nproc)
-	sudo make altinstall
-
-	# Clean .tgz archive
-    cd ..
-    rm -rf Python-$desired_version Python-$desired_version.tgz
+	sudo add-apt-repository -y ppa:deadsnakes/ppa
+    sudo apt-get update
+    sudo apt-get install -y python3.9
 else
 	echo "Correct python3 version installed. [Python3 $desired_version]"
 fi
