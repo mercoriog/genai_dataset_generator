980338384edd335f72bd9c11d244ef4d29194b58
mercoriog <98778953+mercoriog@users.noreply.github.com>
2024-05-08 15:08:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,77 +3,78 @@
 # Check if python is installed:
 if ! command -v python3 &> /dev/null
 then
-	echo "Python3 not installed. Start installation..."
+	echo "[SETUP] Python3 not installed. Start installation..."
 	
 	# Install python using apt.
 	sudo apt install python3==3.9
 
-	echo "Python3 installation done."
+	echo "[SETUP] Python3 installation done."
 else
-	echo "Python3 found."
+	echo "[SETUP] Python3 found."
 fi
 
-# Check python version.
+# Retrive current python version.
 python_version=$(python3 --version 2>&1)
-echo "Current python3 version: $python_version"
+echo "[SETUP] Current python3 version: $python_version"
 
 # Check if pip3 is installed.
 if ! command -v pip3 &> /dev/null
 then
-	echo "pip3 not installed. Start installation..."
+	echo "[SETUP] pip3 not installed. Start installation..."
 
 	# Install pip3.
 	sudo apt install python3-pip
 
-	echo "pip3 installation done."
+	echo "[SETUP] pip3 installation done."
 else
-	echo "pip3 found."
+	echo "[SETUP] pip3 found."
 fi
 
-# Check venv installed
+# Check if venv installed:
 if dpkg -s python3-venv &> /dev/null; then
-    echo "venv found."
+    echo "[SETUP] venv found."
 else
-    echo "venv non installed. Start installation..."
+    echo "[SETUP] venv non installed. Start installation..."
+    # Install venv. 
     pip install --user virtualenv
-    echo "venv installation done."
+    echo "[SETUP] venv installation done."
 fi
 
+# Build virtual environment.
 python3 -m venv genaienv
 
+# Activate virtual environment.
 source genaienv/bin/activate
 
 # Correct python version.
 desired_version="3.9.18"
 
-# Check if python version is 3.9.18
+# Check if python version is '3.9.18':
 if [[ "$python_version" != *"$desired_version"* ]]; then
-	echo "Python $desired_version required. Start installation..."
+	echo "[SETUP] Python $desired_version required. Start installation..."
 	
-	# Install curl
+	# Install curl.
 	sudo apt install curl
 
-    # Install pyenv
+    # Install pyenv.
     curl https://pyenv.run | bash
     export PATH="$HOME/.pyenv/bin:$PATH"
     eval "$(pyenv init --path)"
 
+    # Install python desired version.
     pyenv install $desired_version
     pyenv global $desired_version
 
-	# Install desired python version
-	# pyenv install 3.9.18
-	# Install pipenv
-	# pip install pipenv
-	# Create virtual env with pipenv
-	# pipenv --python $(pyenv which python.3.9.18)
+	echo "[SETUP] Python $desired_version installation done."
 
 else
-	echo "Correct python3 version installed. [Python3 $desired_version]"
+	echo "[SETUP] Correct python3 version installed. [Python3 $desired_version]"
 fi
 
+echo "[SETUP] Start downloading dependencies..."
+# Install dependencies.
 pip install -r requirements.txt
+echo "[SETUP] Dependencies installed"
 
-echo "Done."
-
-sleep 5s
+echo "[SETUP] Done."
+sleep 10s
