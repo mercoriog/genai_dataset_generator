5c2a614bb405a3fdc714fa5ed0e95b9cdb852f3b
mercoriog <98778953+mercoriog@users.noreply.github.com>
2024-05-10 15:36:11 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -87,7 +87,6 @@ curl https://pyenv.run | bash
 export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-
 eval "$(pyenv virtualenv-init -)"
 
 # Install python desired version.
