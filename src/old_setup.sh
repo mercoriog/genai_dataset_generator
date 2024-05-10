243007cb8ff41ee35cab08ae20e36d886d311655
mercoriog <98778953+mercoriog@users.noreply.github.com>
2024-05-10 20:20:29 +0200

    Update old_setup.sh

--- src/old_setup.sh
+++ src/old_setup.sh
@@ -1,23 +1,11 @@
-# Check if python version is '3.9.18':
-if [[ "$python_version" != *"$desired_version"* ]]; then
-	
+#!/bin/bash
 
-else
-	echo "[SETUP] Correct python3 version installed. [Python3 $desired_version]"
-fi
+python -m venv genaienv
 
-# Activate virtual environment.
-source "$venv_path/bin/activate"
+source genaienv/Scripts/activate
 
-# Check if virtual environment is activated successfully:
-if [[ $VIRTUAL_ENV != "" ]]; then
-    echo "[SETUP] Virtual environment activated."
-else
-    echo "[SETUP] Virtual environment NOT activated."
-    exit 1
-fi
-
-echo "[SETUP] Start downloading dependencies..."
-# Install dependencies.
 pip install -r requirements.txt
-echo "[SETUP] Dependencies installed"
+
+echo "Done."
+
+sleep 5s
